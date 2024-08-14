283ed233c0c13293ee204e45e9ef8ec86b1534aa
Oppicer <kadenw2045@gmail.com>
2024-08-14 14:10:15 -0700

    exported from Skill Struck

---
+++ Dictionaries.py
@@ -0,0 +1,9 @@
+mountains = {
+    "Vesuvius" : 4,
+    "Timpanogos" : 3,
+    "Kilimanjaro" : 2,
+    "Everest" : 1
+}
+print (mountains)
+print (mountains["Vesuvius"])
+mountains["Kilimanjaro"] = 6
